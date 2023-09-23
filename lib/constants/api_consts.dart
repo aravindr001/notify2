452ad12c9de518d8ec57e73ffb6db2c1d6d1fc9e
Aravind R <120251962+aravindr001@users.noreply.github.com>
2023-09-23 19:48:11 +0530

    about api key

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,8 @@
 String BASE_URL = "https://api.openai.com/v1";
+
+// paste the api key which you have copied from the 
+// GOOGLE DOCX inside the double quotes
+// eg:   String API_KEY = "pasteAPIkeyHere";
 String API_KEY = "";
+
+
